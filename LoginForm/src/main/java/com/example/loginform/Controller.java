fd463aea4668cd8725d02742c368522f4450c59c
JunBumHan <2hanjunbum6@gmail.com>
2022-11-05 20:06:05 +0900

    Login

--- LoginForm/src/main/java/com/example/loginform/Controller.java
+++ LoginForm/src/main/java/com/example/loginform/Controller.java
@@ -8,6 +8,10 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Controller {
     @FXML
     private Button cancleButton;
@@ -20,7 +24,8 @@ public class Controller {
 
     public void loginButtonOnAction(ActionEvent e) {
         if(usernameTextField.getText().isBlank() == false && passwordPasswordFiled.getText().isBlank() == false) {
-            loginMessageLabel.setText(usernameTextField.getText() + "님 안녕하세요!");
+//
+            validdataLogin();
         } else {
             loginMessageLabel.setText("error");
         }
@@ -30,6 +35,31 @@ public class Controller {
     public void cancleButtonOnAction(ActionEvent e) {
         Stage stage = (Stage) cancleButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void validdataLogin() {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getDatabase();
+
+
+//      SELECT count(1) FROM useraccounts WHERE username = 'JunBumHan' AND password = '1234';
+        String verifyLogin = "SELECT count(1) FROM useraccounts WHERE username = '"+ usernameTextField.getText() + "' AND password = '" + passwordPasswordFiled.getText() + "'";
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+
+            while(queryResult.next()) {
+                if(queryResult.getInt(1) == 1) {
+                    loginMessageLabel.setText("Welcome!");
+                } else {
+                    loginMessageLabel.setText("Invalid Login.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 
--- LoginForm/src/main/java/module-info.java
+++ LoginForm/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.loginform {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
+    requires java.sql;
 
     opens com.example.loginform to javafx.fxml;
     exports com.example.loginform;
